a6f0957a710e6837627bb11015bce15d851d30e7
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-01-13 01:42:32 +0530

    Update helper.py

--- Downloader/modules/helper.py
+++ Downloader/modules/helper.py
@@ -176,7 +176,7 @@ async def download_video(url,cmd, name):
         return os.path.isfile.splitext[0] + "." + "mp4"
 
 
-async def send_doc(m: message,cc,ka,cc1,prog,count,name):
+async def send_doc(m: Message,cc,ka,cc1,prog,count,name):
     reply = await m.reply_text(f"Uploading » `{name}`")
     time.sleep(1)
     start_time = time.time()
@@ -188,7 +188,7 @@ async def send_doc(m: message,cc,ka,cc1,prog,count,name):
     time.sleep(3) 
 
 
-async def send_vid(m: message,cc,filename,thumb,name,prog):
+async def send_vid(m: Message,cc,filename,thumb,name,prog):
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
     await prog.delete (True)
     reply = await m.reply_text(f"**⥣ Uploading ...** » `{name}`")
