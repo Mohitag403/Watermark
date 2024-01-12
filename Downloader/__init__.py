91104f5a0acd7f17d11f92a4708b3b793f946241
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-01-13 00:23:02 +0530

    Add files via upload

--- Downloader/__init__.py
+++ Downloader/__init__.py
@@ -1,24 +1,42 @@
 import asyncio
+import logging
+import time
+from dotenv import load_dotenv
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
 
 
 loop = asyncio.get_event_loop()
+load_dotenv()
+boot = time.time()
 
 
-app = Client(
-    ":Downloader:",
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.INFO,
+)
+
+
+
+Michiko = Client(
+    ":Michiko:",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
 )
 
 
-async def info_bot():
+
+
+
+
+
+
+async def Michiko_bot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
-    await app.start()
-    getme = await app.get_me()
+    await Michiko.start()
+    getme = await Michiko.get_me()
     BOT_ID = getme.id
     BOT_USERNAME = getme.username
     if getme.last_name:
@@ -27,6 +45,6 @@ async def info_bot():
         BOT_NAME = getme.first_name
 
 
-loop.run_until_complete(info_bot())
+loop.run_until_complete(Michiko_bot())
 
 
