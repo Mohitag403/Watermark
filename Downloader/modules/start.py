debdc609567135f19d66bbca519be6cd730102c3
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-01-13 00:28:42 +0530

    Update start.py

--- Downloader/modules/start.py
+++ Downloader/modules/start.py
@@ -3,6 +3,6 @@ from Downloader import app
 
 
 
-@app.on_message(filters.command(""))
+@app.on_message(filters.command("start"))
 async def start(_,message):
   await message.reply_text("i am working sensei !!")
