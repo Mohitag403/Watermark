62a010511285fe79afeffe6692be0610807ede1b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-01-13 00:24:19 +0530

    Update __init__.py

--- Downloader/__init__.py
+++ Downloader/__init__.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import time
 from dotenv import load_dotenv
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
@@ -9,7 +8,7 @@ from config import API_ID, API_HASH, BOT_TOKEN
 
 loop = asyncio.get_event_loop()
 load_dotenv()
-boot = time.time()
+
 
 
 logging.basicConfig(
@@ -19,8 +18,8 @@ logging.basicConfig(
 
 
 
-Michiko = Client(
-    ":Michiko:",
+app = Client(
+    ":Downloader:",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
@@ -33,10 +32,10 @@ Michiko = Client(
 
 
 
-async def Michiko_bot():
+async def info_bot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
-    await Michiko.start()
-    getme = await Michiko.get_me()
+    await app.start()
+    getme = await app.get_me()
     BOT_ID = getme.id
     BOT_USERNAME = getme.username
     if getme.last_name:
@@ -45,6 +44,6 @@ async def Michiko_bot():
         BOT_NAME = getme.first_name
 
 
-loop.run_until_complete(Michiko_bot())
+loop.run_until_complete(info_bot())
 
 
