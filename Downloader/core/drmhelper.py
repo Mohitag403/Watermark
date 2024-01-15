043a8d37f8fdfb3fdbbdef210261a874c22cb9ee
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-01-15 12:29:21 +0530

    Update drmhelper.py

--- Downloader/core/drmhelper.py
+++ Downloader/core/drmhelper.py
@@ -7,11 +7,9 @@ import datetime
 import subprocess
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
-from helpers.toolkit import Tools, Vidtools
 from helpers.prog_bar import progress_for_pyrogram
 from Downloader import app
-
-
+from Downloader.core import helper
 
 
 
@@ -29,9 +27,9 @@ class upload_tg:
 
     async def get_thumb_duration(self):
         try:
-            duration = Vidtools.get_duration(self.file_path)
+            duration = helper.get_duration(self.file_path)
         except:
-            duration = int(Tools.duration(self.file_path))
+            duration = int(helper.duration(self.file_path))
 
         if self.thumb.startswith(("http://", "https://")):
             wget.download(self.thumb, f"{self.temp_dir}.jpg")
@@ -40,28 +38,17 @@ class upload_tg:
             thumbnail = self.thumb
         else:
             try:
-                thumbnail = await Vidtools.take_screen_shot(self.file_path, self.name, self.path, (duration / 2))
+                thumbnail = await helper.take_screen_shot(self.file_path, self.name, self.path, (duration / 2))
             except:
                 subprocess.run(
                     f'ffmpeg -i "{self.filename}" -ss 00:00:01 -vframes 1 "{self.temp_dir}.jpg"', shell=True)
                 thumbnail = f"{self.temp_dir}.jpg"
         return duration, thumbnail
-
-    async def get_doc_thumb(self):
-        if self.thumb.startswith(("http://", "https://")):
-            wget.download(self.thumb, f"{self.temp_dir}.jpg")
-            doc_thumbnail = f"{self.temp_dir}.jpg"
-        elif os.path.isfile(self.thumb):
-            doc_thumbnail = self.thumb
-        else:
-            # wget.download(Extra_info.THUMB_URL, f"{path}/{name}.jpg")
-            # doc_thumbnail = f"{path}/{name}.jpg"
-            doc_thumbnail = None
-        return doc_thumbnail
+    
 
     async def upload_video(self):
         duration, thumbnail = await upload_tg.get_thumb_duration(self)
-        w, h = await Vidtools.get_width_height(self.file_path)
+        w, h = await helper.get_width_height(self.file_path)
         start_time = time.time()
         try:
             await self.app.send_video(
@@ -92,21 +79,4 @@ class upload_tg:
         os.remove(self.file_path)
         await self.show_msg.delete(True)
 
-    async def upload_doc(self):
-        start_time = time.time()
-        try:
-            await self.app.send_document(
-                chat_id=self.message.chat.id,
-                document=self.file_path,
-                caption=self.caption,
-                thumb=await upload_tg.get_doc_thumb(self),
-                progress=progress_for_pyrogram,
-                progress_args=("<b>Uploading :- </b> `{file_name}`".format(             
-                        file_name=f"{self.name}"), self.show_msg, start_time
-                )
-            )
-        except Exception as e:
-        os.remove(self.file_path)
-        await self.show_msg.delete(True)
-
-
+    
