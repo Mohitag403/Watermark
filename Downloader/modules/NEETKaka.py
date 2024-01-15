bf49d0512273536dace99feaed2572bb4fec212e
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-01-15 23:35:19 +0530

    Create NEETKaka.py

---
+++ Downloader/modules/NEETKaka.py
@@ -0,0 +1,181 @@
+import json
+from pyrogram import filters
+from pyrogram.types import Message
+from pyromod import listen
+from pyrogram import Client
+import cloudscraper
+import tgcrypto
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+from base64 import b64decode
+import requests
+from Downloader import app
+from config import SUDO_USERS
+
+
+@app.on_message(filters.command(["nkj"]) & filters.user(SUDO_USERS))
+async def account_login(_, message):
+    global cancel
+    cancel = False
+    editable = await message.reply_text(
+        "Send **ID & Password** in this manner, otherwise, the bot will not respond.\n\nSend like this: **ID*Password**"
+    )
+    raw_url = "https://neetkakajeeapi.classx.co.in/post/userLogin"
+    hdr = {
+        "Auth-Key": "appxapi",
+        "User-Id": "-2",
+        "Authorization": "",
+        "User_app_category": "",
+        "Language": "en",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept-Encoding": "gzip, deflate",
+        "User-Agent": "okhttp/4.9.1"
+    }
+
+    info = {"email": "", "password": ""}
+    input1: Message = await app.listen(editable.chat.id)
+    raw_text = input1.text
+    info["email"] = raw_text.split("*")[0]
+    info["password"] = raw_text.split("*")[1]
+    await input1.delete(True)
+
+    scraper = cloudscraper.create_scraper()
+    res = scraper.post(raw_url, data=info, headers=hdr).content
+    output = json.loads(res)
+
+    userid = output["data"]["userid"]
+    token = output["data"]["token"]
+
+    hdr1 = {
+            "Host": "neetkakajeeapi.classx.co.in",
+            "Client-Service": "Appx",
+            "Auth-Key": "appxapi",
+            "User-Id": userid,
+            "Authorization": token
+            }
+
+    await editable.edit("**login Successful**")
+    res1 = requests.get("https://neetkakajeeapi.classx.co.in/get/mycourseweb?userid="+userid, headers=hdr1)
+    b_data = res1.json()['data']
+    cool = ""
+    for data in b_data:
+        t_name =data['course_name']
+        FFF = "BATCH-ID - BATCH NAME - INSTRUCTOR"
+        aa = f" {data['id']}      - {data['course_name']}**\n\n"
+        if len(f'{cool}{aa}') > 4096:
+            print(aa)
+            cool = ""
+        cool += aa
+    await editable.edit(f'{"**You have these batches :-"}\n\n{FFF}\n\n{cool}')
+    editable1 = await message.reply_text("**Now send the Batch ID to Download**")
+    input2 = message = await app.listen(editable.chat.id)
+    raw_text2 = input2.text
+
+    scraper = cloudscraper.create_scraper()
+    html = scraper.get("https://neetkakajeeapi.classx.co.in/get/allsubjectfrmlivecourseclass?courseid={raw_text2}",headers=hdr1).content
+    output0 = json.loads(html)
+    subjID = output0["data"]
+    await message.reply_text(subjID)
+
+    editable1 = await message.reply_text("**Enter the Subject Id Show in above Response")
+    input3 = message = await app.listen(editable.chat.id)
+    raw_text3 = input3.text
+
+    res3 = requests.get("https://neetkakajeeapi.classx.co.in/get/alltopicfrmlivecourseclass?courseid=" + raw_text2,"&subjectid=" + raw_text3, headers=hdr1)
+    b_data2 = res3.json()['data']
+    vj = ""
+    for data in b_data2:
+        tids = (data["topicid"])
+        idid = f"{tids}&"
+        if len(f"{vj}{idid}") > 4096:
+            vj = ""
+        vj += idid
+    vp = ""
+    for data in b_data2:
+        tn = (data["topic_name"])
+        tns = f"{tn}&"
+        if len(f"{vp}{tn}") > 4096:
+            vp = ""
+        vp += tns
+    cool1 = ""
+    for data in b_data2:
+        t_name = (data["topic_name"])
+        tid = (data["topicid"])
+        zz = len(tid)
+        BBB = f"{'**TOPIC-ID    - TOPIC     - VIDEOS**'}\n"
+        hh = f"```{tid}```     - **{t_name} - ({zz})**\n"
+        if len(f'{cool1}{hh}') > 4096:
+            cool1 = ""
+        cool1 += hh
+    await m.reply_text(f'Batch details of **{t_name}** are:\n\n{BBB}\n\n{cool1}')
+    editable= await message.reply_text(f"Now send the **Topic IDs** to Download\n\nSend like this **1&2&3&4** so on\nor copy paste or edit **below ids** according to you :\n\n**Enter this to download full batch :-**\n```{vj}```")
+    input4 = message = await app.listen(editable.chat.id)
+    raw_text4 = input4.text
+
+    editable3 = await message.reply_text("**Now send the Resolution**")
+    input5 = message = await app.listen(editable.chat.id)
+    raw_text5 = input5.text
+    try:
+        xv = raw_text4.split('&')
+        for y in range(0,len(xv)):
+            t =xv[y]
+
+            hdr11 = {
+                    "Host": "neetkakajeeapi.classx.co.in",
+                    "Client-Service": "Appx",
+                    "Auth-Key": "appxapi",
+                    "User-Id": userid,
+                    "Authorization": token
+                    }
+
+            res4 = requests.get("https://neetkakajeeapi.classx.co.in/get/livecourseclassbycoursesubtopconceptapiv3?topicid=" + t + "&start=-1&courseid=" + raw_text2 + "&subjectid=" + raw_text3,headers=hdr11).json()
+
+            topicid = res4["data"]
+            vj = ""
+            for data in topicid:
+                tids = (data["Title"])
+                idid = f"{tids}"
+                if len(f"{vj}{idid}") > 4096:
+                    vj = ""
+                vj += idid
+
+            vp = ""
+            for data in topicid:
+                tn = (data["download_link"])
+                tns = f"{tn}"
+                if len(f"{vp}{tn}") > 4096:
+                    vp = ""
+                vp += tn
+            vs = ""
+            for data in topicid:
+                tn0 = (data["pdf_link"])
+                tns0 = f"{tn0}"
+                if len(f"{vs}{tn0}") > 4096:
+                    vs = ""
+                vs += tn0
+            cool2 = ""
+            for data in topicid:
+                if data["download_link"]:
+                    b64 = (data["download_link"])
+                else:
+                    b64 = (data["pdf_link"])
+                tid = (data["Title"])
+                zz = len(tid)
+                key = "638udh3829162018".encode("utf8")
+                iv = "fedcba9876543210".encode("utf8")
+                ciphertext = bytearray.fromhex(b64decode(b64.encode()).hex())
+                cipher = AES.new(key, AES.MODE_CBC, iv)
+                plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
+                b=plaintext.decode('utf-8')
+                cc0 = (f"{tid}:{b}")
+                if len(f'{cool2}{cc0}') > 4096:
+                    cool2 = ""
+                cool2 += cc0
+                mm = "NEET Kaka JEE"
+                
+                with open(f'{mm}.txt', 'a') as f:
+                    f.write(f"{tid}:{b}\n")
+        await message.reply_document(f"{mm}.txt")
+    except Exception as e:
+        await message.reply_text(str(e))
+    await message.reply_text("Done")
