ca9499aeae2f25070680078d98f79c3605970e4d
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-01-16 15:16:44 +0530

    Update neetkakajee.py

--- Downloader/modules/neetkakajee.py
+++ Downloader/modules/neetkakajee.py
@@ -108,7 +108,7 @@ async def neetkaka_login(_, message):
     
         lol += hh
     
-    await message.reply_text(f"**TOPIC-ID    - TOPIC     - VIDEOS**\n\n{lol}")
+    await message.reply_text(f"Batch details of **{t_name}** are:\n**TOPIC-ID    - TOPIC     - VIDEOS**\n\n{lol}")
 
     
     editable = await message.reply_text(f"Now send the **Topic IDs** to Download\n\nSend like this **1&2&3&4** so on\nor copy paste or edit **below ids** according to you :\n\n**Enter this to download full batch :-**\n```{vj}```")
