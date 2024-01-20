5abc66832e044ea55ea3acce92c9ea951696afdc
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-01-20 22:42:02 +0530

    Update neetkakajee.py

--- Downloader/modules/neetkakajee.py
+++ Downloader/modules/neetkakajee.py
@@ -152,12 +152,12 @@ async def neetkaka_login(_, message):
                         print(f"Unexpected format: {encoded_string}")
                     cool2 = f"{b}"
  
-            mm = "NEET Kaka JEE"     
-            with open(f'{mm}.txt', 'a') as f:
-                f.write(f"{vj} : {cool2}\n {vs}")
-            await message.reply_document(f"{mm}.txt")
-            file_path = f"{mm}.txt"
-            os.remove(file_path)
+                mm = "NEET Kaka JEE"     
+                with open(f'{mm}.txt', 'a') as f:
+                    f.write(f"{vj} : {cool2}\n {vs}")
+                await message.reply_document(f"{mm}.txt")
+                file_path = f"{mm}.txt"
+                os.remove(file_path)
   
     except Exception as e:
         await message.reply_text(str(e))
