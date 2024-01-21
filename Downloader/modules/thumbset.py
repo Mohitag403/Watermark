a35be52f56ecae045d6c940e08d918f7ae170773
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-01-21 20:46:26 +0530

    Update thumbset.py

--- Downloader/modules/thumbset.py
+++ Downloader/modules/thumbset.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from Downloader import app
-from Downloader.core.database import db
+from Downloader.core.database import get_thumbnail, set_thumbnail
 
 
 
 @app.on_message(filters.command("viewthumb"))
 async def viewthumb(client, message):    
-    thumb = await db.get_thumbnail(message.from_user.id)
+    thumb = await get_thumbnail(message.from_user.id)
     if thumb:
        await client.send_photo(
 	   chat_id=message.chat.id, 
@@ -17,14 +17,14 @@ async def viewthumb(client, message):
 
 @app.on_message(filters.command("delthumb"))
 async def removethumb(client, message):
-    await db.set_thumbnail(message.from_user.id, file_id=None)
+    await set_thumbnail(message.from_user.id, file_id=None)
     await message.reply_text("❌️ **ʏᴏᴜʀ ᴛʜᴜᴍʙɴᴀɪʟ sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ.**")
 
 
 @app.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
     lol = await message.reply_text("ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...↻")
-    await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
+    await set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
     await lol.edit("✅️**ʏᴏᴜʀ ᴛʜᴜᴍʙɴᴀɪʟ sᴜᴄᴄᴇssғᴜʟʟʏ sᴀᴠᴇᴅ**")
 
 
