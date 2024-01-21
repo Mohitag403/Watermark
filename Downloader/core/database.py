a01ecbb724fbe1a6c6ace10cecebf435da59a2f9
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-01-21 20:54:41 +0530

    Update database.py

--- Downloader/core/database.py
+++ Downloader/core/database.py
@@ -1,20 +1,16 @@
 from config import MONGO_DB
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-
 mongo = MongoCli(MONGO_DB)
 db = mongo.thumbnail
-db = db.users
-
+collection = db.users
 
 
 
 async def set_thumbnail(user_id, file_id):
-    await db.update_one({'user_id': int(id)}, {'$set': {'file_id': file_id}})
+    await collection.update_one({'_id': int(user_id)}, {'$set': {'file_id': file_id}})
 
 async def get_thumbnail(user_id):
-    user = await db.find_one({'user_id': int(id)})
+    user = await collection.find_one({'_id': int(user_id)})
     return user.get('file_id', None)
-
-
 
