7902f3786746c4f73319ba248b78997bd133ca57
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-01-22 14:40:26 +0530

    Update start.py

--- Downloader/modules/start.py
+++ Downloader/modules/start.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @app.on_message(filters.command("start"))
 async def start(_,message):
-  await message.reply_photo(photo="https://telegra.ph/file/9456751a4ca1a346e631f.jpg", caption="**𝙷𝚒!**\n\n**𝙶𝚒𝚟𝚎 /𝚝𝚡𝚝 𝙲𝚘𝚖𝚖𝚊𝚗𝚍 𝚝𝚘 𝙳𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝙵𝚛𝚘𝚖 𝚊 𝚃𝚎𝚡𝚝 𝚏𝚒𝚕𝚎.**\𝚗🎓✨",
+  await message.reply_photo(photo="https://telegra.ph/file/9456751a4ca1a346e631f.jpg", caption="**𝙷𝚒!**\n\n**𝙶𝚒𝚟𝚎 /𝚝𝚡𝚝 𝙲𝚘𝚖𝚖𝚊𝚗𝚍 𝚝𝚘 𝙳𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝙵𝚛𝚘𝚖 𝚊 𝚃𝚎𝚡𝚝 𝚏𝚒𝚕𝚎.**🎓✨",
                             reply_markup=InlineKeyboardMarkup([
                 [
                   InlineKeyboardButton("ʜᴇʟᴘ", callback_data="help_")
