14bdd2bd9e6c712fecccd05ff2105c366186f4a6
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-01-22 14:08:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "18618422"))
 API_HASH = getenv("API_HASH", "f165b1caec3cfa4df943fe1cbe82d22a")
-BOT_TOKEN = getenv("BOT_TOKEN", "6654523286:AAGq-8QNJjv5cQNc_V3HqteBvJonvLZDN6M")
+BOT_TOKEN = getenv("BOT_TOKEN", "6968442211:AAHaOay5WJH_wsycGKYYewvbtrr83HdUOBw")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6050277919 5890592765 5809339058 2112898623").split()))
