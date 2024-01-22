cc5250d902552c4412302b7ac4f071101e35fa08
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-01-22 13:54:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "26850449"))
-API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_TOKEN = getenv("BOT_TOKEN", "6373343474:AAHONd62VYjN69252RNhWDHy6ybEPi-cDEc")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6280048819 6691393517 6050277919 2015585738").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority")
+API_ID = int(getenv("API_ID", "18618422"))
+API_HASH = getenv("API_HASH", "f165b1caec3cfa4df943fe1cbe82d22a")
+BOT_TOKEN = getenv("BOT_TOKEN", "6654523286:AAGq-8QNJjv5cQNc_V3HqteBvJonvLZDN6M")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6050277919 5890592765 5809339058 2112898623").split()))
