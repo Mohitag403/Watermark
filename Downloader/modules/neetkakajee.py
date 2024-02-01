6d5b483b7cc1f5148252f2231d8b3d48c12b788f
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-02-01 14:27:23 +0530

    Update neetkakajee.py

--- Downloader/modules/neetkakajee.py
+++ Downloader/modules/neetkakajee.py
@@ -2,19 +2,25 @@ import json
 import os
 import requests
 from pyrogram import filters
+from main import LOGGER, prefixes
+from pyrogram.types import Message
 from pyromod import listen
 import cloudscraper
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 from base64 import b64decode
+import config
 from Downloader import app
-from config import SUDO_USERS
+
+
 def decrypt_data(encoded_data, key, iv):
     decoded_data = b64decode(encoded_data)
     cipher = AES.new(key, AES.MODE_CBC, iv)
     decrypted_data = unpad(cipher.decrypt(decoded_data), AES.block_size)
     return decrypted_data.decode('utf-8')
-@app.on_message(filters.command(["nkj"]) & filters.user(SUDO_USERS))
+
+
+@app.on_message(filters.command(["nkj"]))
 async def neetkaka_login(_, message):
     global cancel
     cancel = False
@@ -33,7 +39,7 @@ async def neetkaka_login(_, message):
         "User-Agent": "okhttp/4.9.1"
     }
     info = {"email": "", "password": ""}
-    input1: message = await _.listen(editable.chat.id)
+    input1: message = await bot.listen(editable.chat.id)
     raw_text = input1.text
     info["email"] = raw_text.split("*")[0]
     info["password"] = raw_text.split("*")[1]
@@ -64,7 +70,7 @@ async def neetkaka_login(_, message):
         cool += aa
     await editable.edit(f'{"**You have these batches :-"}\n\n{FFF}\n\n{cool}')
     editable = await message.reply_text("**Now send the Batch ID to Download**")
-    input2: message = await _.listen(editable.chat.id)
+    input2: message = await bot.listen(editable.chat.id)
     raw_text2 = input2.text
     scraper = cloudscraper.create_scraper()
     html = scraper.get(f"https://neetkakajeeapi.classx.co.in/get/allsubjectfrmlivecourseclass?courseid={raw_text2}",headers=hdr1).content
@@ -80,7 +86,7 @@ async def neetkaka_login(_, message):
     await editable.edit(cool)
 
     editable = await message.reply_text("**Enter the Subject Id Show in above Response")
-    input3: message = await _.listen(editable.chat.id)
+    input3: message = await bot.listen(editable.chat.id)
     raw_text3 = input3.text
     res3 = requests.get("https://neetkakajeeapi.classx.co.in/get/alltopicfrmlivecourseclass?courseid=" +raw_text2+"&subjectid="+raw_text3, headers=hdr1)
     b_data2 = res3.json()['data']
@@ -105,14 +111,13 @@ async def neetkaka_login(_, message):
     await message.reply_text(f"Batch details of **{t_name}** are:\n\n{BBB}\n\n{lol}")
 
     editable = await message.reply_text(f"Now send the **Topic IDs** to Download\n\nSend like this **1&2&3&4** so on\nor copy paste or edit **below ids** according to you :\n\n**Enter this to download full batch :-**\n`{vj}`")
-    input4: message = await _.listen(editable.chat.id)
+    input4: message = await bot.listen(editable.chat.id)
     raw_text4 = input4.text
     editable = await message.reply_text("**Now send the Resolution**")
-    input5: message = await _.listen(editable.chat.id)
+    input5: message = await bot.listen(editable.chat.id)
     raw_text5 = input5.text
+
     vj = ""
-    vk = ""
-    vm = ""
     try:
         xv = raw_text4.split('&')
         for y in range(0,len(xv)):
@@ -129,45 +134,43 @@ async def neetkaka_login(_, message):
 
             for data in topicid:
                 tids = (data["Title"])
-                idid = f"{tids}"
-                if len(f"{vj}{idid}") > 4096:
-                    vj = ""
-                vj += idid 
-
                 plinks = [data["pdf_link"]]
-                key = "638udh3829162018".encode("utf8")
-                iv = "fedcba9876543210".encode("utf8")
-                for plink in plinks:
-                    parts = plink.split(':')
-                    if len(parts) == 2:
-                        encoded_part, encrypted_part = parts
-                        bp = decrypt_data(encoded_part, key, iv)
-                    else:
-                        print(f"Unexpected format: {encoded_string}")
-                    vs = f"{bp}"
-                if len(f"{vk}{vs}") > 4096:
-                    vk = ""
-                vk += vs 
                 dlinks = [link['path'] for link in data['download_links'] if link['quality'] == f"{raw_text5}p"]
                 key = "638udh3829162018".encode("utf8")
                 iv = "fedcba9876543210".encode("utf8")
-                for dlink in dlinks:
-                    parts = dlink.split(':')
-                    if len(parts) == 2:
-                        encoded_part, encrypted_part = parts
-                        b = decrypt_data(encoded_part, key, iv)
-                    else:
-                        print(f"Unexpected format: {encoded_string}")
-                    cool2 = f"{b}"
-                if len(f"{vm}{cool2}") > 4096:
-                    vm = ""
-                vm += cool2
-                mm = "NEET Kaka JEE"     
-                with open(f'{mm}.txt', 'a') as f:
-                    f.write(f"{vj} : {vm}\n {vk}")
-                await message.reply_document(f"{mm}.txt")
-                file_path = f"{mm}.txt"
-                os.remove(file_path)
+
+                if plinks and dlinks:
+                    idid = f"{tids}"
+                    for plink in plinks:
+                        parts = plink.split(':')
+                        if len(parts) == 2:
+                            encoded_part, encrypted_part = parts
+                            bp = decrypt_data(encoded_part, key, iv)
+                            vs = f"{bp}"
+                        else:
+                            print(f"Unexpected format: {plink}")
+                            continue
+                    for dlink in dlinks:                    
+                        parts = dlink.split(':')
+                        if len(parts) == 2:
+                            encoded_part, encrypted_part = parts
+                            b = decrypt_data(encoded_part, key, iv)
+                            cool2 = f"{b}"
+                        else:
+                            print(f"Unexpected format: {dlink}")
+                            continue
+  
+                    msg = f"{idid} : {cool2}\n{idid} : {vs}\n\n"
+                    vj += msg
+                else:
+                    return
+                
+            mm = "NEET Kaka JEE"     
+            with open(f'{mm}.txt', 'a') as f:
+                f.write(f"{vj}")
+            await message.reply_document(f"{mm}.txt")
+            file_path = f"{mm}.txt"
+            os.remove(file_path)
 
     except Exception as e:
         await message.reply_text(str(e))
