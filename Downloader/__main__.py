5e6440130445a2b1b0a703369b5aa028b1a9419a
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-02-23 17:37:14 +0530

    Update __main__.py

--- Downloader/__main__.py
+++ Downloader/__main__.py
@@ -2,13 +2,11 @@ import asyncio
 import importlib
 from pyrogram import idle
 from Downloader.modules import ALL_MODULES
-from Downloader.modules.database import setup_sudoers
 
 async def sumit_boot():
     for all_module in ALL_MODULES:
         importlib.import_module("Downloader.modules." + all_module)
 
-    await setup_sudoers()
     print("» ʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ ✨ 🎉")
     await idle()
     print("» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
