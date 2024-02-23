5b2d300aff8f4d691ee0fe30fd3a309a407e6feb
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-02-23 21:40:52 +0530

    Update and rename database.py to database.py

--- Downloader/core/database.py
+++ Downloader/core/database.py
@@ -43,41 +43,26 @@ async def extract_user(m: Message) -> User:
     )
 
 
-async def get_sudoers() -> list[int]:
-    try:
-        sudoers_doc = await sudoersdb.find_one({"sudo": "sudo"})
-        if sudoers_doc:
-            return sudoers_doc.get("sudoers", [])
-        else:
-            return []
-    except Exception as e:
-        print(f"Error fetching sudoers: {str(e)}")
+async def get_sudoers() -> list:
+    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    if not sudoers:
         return []
+    return sudoers["sudoers"]
+
 
 async def add_sudo(user_id: int) -> bool:
-    try:
-        sudoers = await get_sudoers()
-        if user_id in sudoers:
-            return False  # User is already a sudoer
-        sudoers.append(user_id)
-        await sudoersdb.update_one(
-            {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
-        )
-        return True
-    except Exception as e:
-        print(f"Error adding sudo: {str(e)}")
-        return False
+    sudoers = await get_sudoers()
+    sudoers.append(user_id)
+    await sudoersdb.update_one(
+        {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
+    )
+    return True
+
 
 async def remove_sudo(user_id: int) -> bool:
-    try:
-        sudoers = await get_sudoers()
-        if user_id not in sudoers:
-            return False  # User is not a sudoer
-        sudoers.remove(user_id)
-        await sudoersdb.update_one(
-            {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
-        )
-        return True
-    except Exception as e:
-        print(f"Error removing sudo: {str(e)}")
-        return False
+    sudoers = await get_sudoers()
+    sudoers.remove(user_id)
+    await sudoersdb.update_one(
+        {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
+    )
+    return True
