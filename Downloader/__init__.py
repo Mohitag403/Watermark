04ae7089fb9793225052afc5100ab67e01ab68d7
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-02-23 16:51:44 +0530

    Update __init__.py

--- Downloader/__init__.py
+++ Downloader/__init__.py
@@ -1,39 +1,21 @@
-import asyncio, config, logging
-from pyromod import listen
+# __init__.py
+
+import asyncio
+import logging
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
-from Downloader.modules.sudoers import mongodb
-from Downloader.modules.sudoers import *
-
-
-loop = asyncio.get_event_loop()
-
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
-    level=logging.INFO,
-)
-
-app = Client(
-    ":Downloader:",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-)
-
-async def setup_sudoers():
-    global SUDOERS
-    SUDOERS = set()
-    SUDOERS.add(config.OWNER_ID)  # Assuming config.OWNER_ID is defined elsewhere
-    sudoers = await sudoers_module.get_sudoers()
-    if config.OWNER_ID not in sudoers:
-        sudoers.append(config.OWNER_ID)
-        await sudoers_module.set_sudoers(sudoers)
-    for user_id in sudoers:
-        SUDOERS.add(user_id)
+from Downloader.modules.sudoers import setup_sudoers
 
 
 async def info_bot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
+    app = Client(
+        ":Downloader:",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN,
+    )
+
     await app.start()
     getme = await app.get_me()
     BOT_ID = getme.id
@@ -45,6 +27,5 @@ async def info_bot():
     await setup_sudoers()
 
 
+loop = asyncio.get_event_loop()
 loop.run_until_complete(info_bot())
-
-
