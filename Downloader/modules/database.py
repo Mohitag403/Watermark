534e9287aacf3d7088f2efaf3edea8b2173a6e53
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-02-23 18:14:45 +0530

    Update database.py

--- Downloader/modules/database.py
+++ Downloader/modules/database.py
@@ -2,7 +2,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import config
 from config import MONGO_DB_URI
 from pyrogram import filters
-
+from pyrogram.enums import MessageEntityType
+from pyrogram.types import Message, User
 
 mongo_client = AsyncIOMotorClient(MONGO_DB_URI)
 mongodb = mongo_client.TXT
@@ -27,6 +28,19 @@ async def setup_sudoers():
     if sudoers:
         for user_id in sudoers:
             SUDOERS.add(user_id)
+
+
+async def extract_user(m: Message) -> User:
+    if m.reply_to_message:
+        return m.reply_to_message.from_user
+    msg_entities = m.entities[1] if m.text.startswith("/") else m.entities[0]
+    return await app.get_users(
+        msg_entities.user.id
+        if msg_entities.type == MessageEntityType.TEXT_MENTION
+        else int(m.command[1])
+        if m.command[1].isdecimal()
+        else m.command[1]
+    )
 
 
 async def get_sudoers() -> list[int]:
