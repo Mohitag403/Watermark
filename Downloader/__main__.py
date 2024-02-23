5504f0d48f5e6fee9dac1374c3aefabcc2aecc21
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-02-23 15:36:34 +0530

    Update __main__.py

--- Downloader/__main__.py
+++ Downloader/__main__.py
@@ -2,7 +2,7 @@ import asyncio
 import importlib
 from pyrogram import idle
 from Downloader.modules import ALL_MODULES
-
+from Downloader.modules.sudoers import mongodb
  
 
 loop = asyncio.get_event_loop()
@@ -16,6 +16,22 @@ async def sumit_boot():
     await idle()
     print("» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
 
+async def sudo():
+    global SUDOERS
+    SUDOERS.add(config.OWNER_ID)
+    sudoersdb = mongodb.sudoers
+    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    sudoers = [] if not sudoers else sudoers["sudoers"]
+    if config.OWNER_ID not in sudoers:
+        sudoers.append(config.OWNER_ID)
+        await sudoersdb.update_one(
+            {"sudo": "sudo"},
+            {"$set": {"sudoers": sudoers}},
+            upsert=True,
+        )
+    if sudoers:
+        for user_id in sudoers:
+            SUDOERS.add(user_id)
 
 if __name__ == "__main__":
     loop.run_until_complete(sumit_boot())
