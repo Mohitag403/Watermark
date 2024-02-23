a6a4e18c4d1defbd2a6db2b423d8fa5aeffec18f
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2024-02-23 16:39:47 +0530

    Update __init__.py

--- Downloader/__init__.py
+++ Downloader/__init__.py
@@ -1,12 +1,11 @@
-import asyncio
-import logging
+import asyncio, config, logging
 from pyromod import listen
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 from Downloder import app
 from Downloader.modules.sudoers import mongodb
 from Downloader.modules.sudoers import *
-import config
+
 
 loop = asyncio.get_event_loop()
 
@@ -15,36 +14,12 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-
-
 app = Client(
     ":Downloader:",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
 )
-
-
-
-
-
-
-
-
-async def info_bot():
-    global BOT_ID, BOT_NAME, BOT_USERNAME
-    await app.start()
-    getme = await app.get_me()
-    BOT_ID = getme.id
-    BOT_USERNAME = getme.username
-    if getme.last_name:
-        BOT_NAME = getme.first_name + " " + getme.last_name
-    else:
-        BOT_NAME = getme.first_name
-
-
-loop.run_until_complete(info_bot())
-
 
 async def setup_sudoers():
     global SUDOERS
@@ -57,5 +32,20 @@ async def setup_sudoers():
     for user_id in sudoers:
         SUDOERS.add(user_id)
 
-# Run the event loop until setup_sudoers coroutine completes
-loop.run_until_complete(setup_sudoers())
+
+async def info_bot():
+    global BOT_ID, BOT_NAME, BOT_USERNAME
+    await app.start()
+    getme = await app.get_me()
+    BOT_ID = getme.id
+    BOT_USERNAME = getme.username
+    if getme.last_name:
+        BOT_NAME = getme.first_name + " " + getme.last_name
+    else:
+        BOT_NAME = getme.first_name
+    await setup_sudoers()
+
+
+loop.run_until_complete(info_bot())
+
+
