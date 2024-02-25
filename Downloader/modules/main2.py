1bc64386da5486c14d5ae89096f3fd141c492dfb
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-02-25 12:42:56 +0530

    Update main2.py

--- Downloader/modules/main2.py
+++ Downloader/modules/main2.py
@@ -47,7 +47,7 @@ async def download_link(links, message, count, raw_text2, res, raw_text0, raw_te
             url =  "https://d26g5bnklkwsh4.cloudfront.net/" + id + "/master.m3u8"
 
         name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
-        name = f'{str(count).zfill(3)}) {name1[:60]}'
+        name = f'{str(count).zfill(3)}) {message.chat.id}{name1[:60]}'
       
 
         if "youtu" in url:
@@ -56,9 +56,9 @@ async def download_link(links, message, count, raw_text2, res, raw_text0, raw_te
             ytf = f"b[height<={raw_text2}]/bv[height<={raw_text2}]+ba/b/bv+ba"
 
         if "jw-prod" in url:
-            cmd = f'yt-dlp -o "{message.chat.id}{name}.mp4" "{url}"'
+            cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
         else:
-            cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{message.chat.id}{name}.mp4"'
+            cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
         try:  
             cc = f'**{str(count).zfill(3)}).  {name1}.mkv {res}** \n\n**Bᴀᴛᴄʜ : {raw_text0}**\n\n**Dᴏᴡɴʟᴏᴀᴅᴇᴅ Bʏ : {raw_text3}**\n\n'
@@ -93,7 +93,7 @@ async def download_link(links, message, count, raw_text2, res, raw_text0, raw_te
             else:
                 show = f"**⥥ Downloading »**\n\n**Name »** `{name}\nQuality » {raw_text2}`\n\n**Url »** `{url}`"
                 prog = await message.reply_text(show)
-                res_file = await helper.download_video(message, url, cmd, name)
+                res_file = await helper.download_video(url, cmd, name)
                 filename = res_file
 #                await prog.delete(True)
                 await helper.send_vid(message, cc, filename, thumb, name, prog)
