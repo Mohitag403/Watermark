a72d95c82d6f5fe75e4ecc349bd4802ea593373f
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-03-06 19:05:15 +0530

    Update settings.py

--- Watermark/modules/settings.py
+++ Watermark/modules/settings.py
@@ -3,27 +3,55 @@ from Watermark.core.mongo import db
 from Watermark import app
 
 
-async def view_thumb(client, message, user_id):    
-    thumb = await db.get_thumbnail(user_id)
+async def view_thumb(client, message):    
+    thumb = await db.get_thumbnail(message.from_user.id)
     if thumb:
-       await client.send_photo(chat_id=message.chat.id, photo=thumb)
+       await client.send_photo(message.chat.id, photo=thumb)
     else:
         await query.answer("**ʏᴏᴜ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴀɴʏ ᴛʜᴜᴍʙɴᴀɪʟ.**", show_alert=True) 
 
 
 
-async def remove_thumb(client, message, user_id):
-    thumb = await db.set_thumbnail(user_id, file_id=None)
+async def remove_thumb(client, message):
+    thumb = await db.set_thumbnail(message.from_user.id, file_id=None)
     if thumb:
       await message.answer("❌️ **ʏᴏᴜʀ ᴛʜᴜᴍʙɴᴀɪʟ sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ.**", show_alert=True)
     else:
       await message.answer("Empty !! Thumbnail", show_alert=True)
 	
 
-async def add_thumb(client, message, user_id):
+async def add_thumb(client, message):
     mkn = await app.ask(message.chat.id, text="Give me your thumbnail photo")
     photo = mkn.text
-    await db.set_thumbnail(user_id, file_id=photo)                
+    await db.set_thumbnail(message.from_user.id, file_id=photo)                
     await mkn.edit("✅️**ʏᴏᴜʀ ᴛʜᴜᴍʙɴᴀɪʟ sᴜᴄᴄᴇssғᴜʟʟʏ sᴀᴠᴇᴅ**")
+
+
+
+
+async def add_caption(client, message):    
+    cap = await app.ask(message.chat.id, text="» ɢɪᴠᴇ ᴍᴇ ᴀ ᴄᴀᴘᴛɪᴏɴ ᴛᴏ sᴇᴛ.")
+    caption = cap.text
+    await db.set_caption(message.from_user.id, caption=caption)
+    await message.reply_text("✅ ʏᴏᴜʀ ᴄᴀᴘᴛɪᴏɴ sᴜᴄᴄᴇssғᴜʟʟʏ.")
+
+    
+
+async def delete_caption(client, message):
+    caption = await db.get_caption(message.from_user.id)  
+    if not caption:
+       await message.answer("ʏᴏᴜ ᴅᴏɴʏ ʜᴀᴠᴇ ᴄᴀᴘᴛɪᴏɴ.", show_alert=True)
+    await db.set_caption(message.from_user.id, caption=None)
+    await message.reply_text(" ʏᴏᴜʀ ᴄᴀᴘᴛɪᴏɴ sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ.")
+                                       
+
+async def see_caption(client, message):
+    caption = await db.get_caption(message.from_user.id)  
+    if caption:
+       await message.reply_text(f"**ʏᴏᴜʀ ᴄᴀᴘᴛɪᴏɴ:**\n\n`{caption}`")
+    else:
+       await message.answer("ʏᴏᴜ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴀᴘᴛɪᴏɴ.", show_alert=True)
+
+
 
 
