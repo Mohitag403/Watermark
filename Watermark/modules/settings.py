03470eb38f4fa620d5960a85e368acc4e2ff77df
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-03-06 22:48:58 +0530

    Update settings.py

--- Watermark/modules/settings.py
+++ Watermark/modules/settings.py
@@ -53,5 +53,58 @@ async def see_caption(query):
        return ("ʏᴏᴜ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴀᴘᴛɪᴏɴ.")
 
 
+buttons1 = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("Thumbnail", callback_data="thumb"),
+                InlineKeyboardButton("Caption", callback_data="caption")
+            ]
+        ])
+
+buttons2 = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("Set Thumbnail", callback_data="Sthumb"),
+                InlineKeyboardButton("Remove Thumbnail", callback_data="Rthumb")
+            ],
+            [
+                InlineKeyboardButton("View Thumbnail", callback_data="Vthumb"),
+            ]
+        ])
+
+buttons3 = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton("Set Caption", callback_data="Scaption"),
+                InlineKeyboardButton("Remove Caption", callback_data="Rcaption")
+            ]
+        ])
+
+
+@app.on_message(filters.command("settings") & filters.private)
+async def settings(_, message):
+    await messgae.reply_text("Choose from Below", reply_markup=buttons1)
+
+
+@app.on_callback_query()
+async def callback(_, query):
+    if query.data=="thumb":
+        await messgae.reply_text("Choose from Below", reply_markup=buttons2)
+
+    elif query.data=="caption":
+        caption = await see_caption(query)
+        await messgae.reply_text(f"Choose from Below\n\n**ʏᴏᴜʀ ᴄᴀᴘᴛɪᴏɴ:** `{caption}`", reply_markup=buttons3)
+
+    elif query.data=="Sthumb":
+        await add_thumb(query)
+
+    elif query.data=="Rthumb":
+        await remove_thumb(query)
+
+    elif query.data=="Vthumb":
+        await view_thumb(query)
+
+    elif query.data=="Scaption":
+        await add_caption(query)
+
+    elif query.data=="Rcaption":
+        await delete_caption(query)
 
 
