aae99d97a95af74653cf9796eb7682a0855227da
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-03-06 16:19:07 +0530

    Update main.py

--- Watermark/modules/main.py
+++ Watermark/modules/main.py
@@ -4,13 +4,15 @@ from config import OWNER_ID
 
 @app.on_message((filters.document | filters.video | filters.photo) & filters.private)
 async def watcher(app, message):
-    if message.photo:
+    if message.photo or (message.document and message.document.mime_type.startswith("photo/")):
         photo = await message.download()
+        await message.reply_text("Yes, it's a photo\nWait downloading...")
         await app.send_photo(chat_id=message.chat.id, photo=photo)
         
     elif message.video or (message.document and message.document.mime_type.startswith("video/")):
-        await message.reply_text("Yes, it's a video")
+        video = await message.download()
+        await message.reply_text("Yes, it's a video\nwait downloading...")        
+        await app.send_video(chat_id=message.chat.id, video=video)                
     else:
-        await message.reply_text("none !!! kuch nhi dono me se")
-
+        pass
 
