b6424432ebd3c42e7a1e299700b95aab295266bb
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-03-07 00:52:27 +0530

    Update db.py

--- Watermark/core/mongo/db.py
+++ Watermark/core/mongo/db.py
@@ -7,8 +7,12 @@ db = mongo.watermark
 db = db.watermark_db
 
 
+async def get_data(user_id):
+    x = await db.find_one({"_id": user_id})
+    return x
+
 async def bot_on_off(x, y):
-    data = await get_thumbnail(user_id)
+    data = await get_data(user_id)
     if data and data.get("_id"):
         await db.update_one({"_id": x}, {"$set": {"Bot": y}})
     else:
@@ -17,41 +21,27 @@ async def bot_on_off(x, y):
 
 
 async def set_thumbnail(user_id, thumb):
-    data = await get_thumbnail(user_id)
+    data = await get_data(user_id)
     if data and data.get("_id"):
         await db.update_one({"_id": user_id}, {"$set": {"thumb": thumb}})
     else:
         await db.insert_one({"_id": user_id, "thumb": thumb})
 
-async def get_thumbnail(user_id):
-    x = await db.find_one({"_id": user_id})
-    return x
-
-
 
 async def set_caption(user_id, caption):
-    data = await get_thumbnail(user_id)
+    data = await get_data(user_id)
     if data and data.get("_id"):
         await db.update_one({"_id": user_id}, {"$set": {"caption": caption}})
     else:
         await db.insert_one({"_id": user_id, "caption": caption})
 
-async def get_caption(user_id):
-    x = await db.find_one({"_id": user_id})
-    return x
-    
-
 
 async def set_watermarl(user_id, watermark):
-    data = await get_thumbnail(user_id)
+    data = await get_data(user_id)
     if data and data.get("_id"):
         await db.update_one({"_id": user_id}, {"$set": {"watermark": watermark}})
     else:
         await db.insert_one({"_id": user_id, "watermark": watermark})
-
-async def get_watermark(user_id):
-    x = await db.find_one({"_id": user_id})
-    return x
 
 
 
