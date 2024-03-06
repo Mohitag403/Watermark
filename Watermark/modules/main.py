d033a03a5d1da62e23f7573ccc88c504a4b5a7bb
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-03-06 17:33:57 +0530

    Update main.py

--- Watermark/modules/main.py
+++ Watermark/modules/main.py
@@ -7,26 +7,15 @@ from config import OWNER_ID
 from Watermark.core.utils import progress_bar
 
 
+
+
 async def dl_send(message):
     reply = await message.reply_text("Yes, it's a video\nwait downloading...")     
     video = await message.download()
-    video_id = message.video.file_id
-    subprocess.run(f'ffmpeg -i "{video}" -ss 00:01:00 -vframes 1 "{video_id}.jpg"', shell=True)
-   # thumbnail = f"{video_id}.jpg"
-    dur = int(duration(video))
-    start_time = time.time() 
-#    await app.send_video(chat_id=message.chat.id, video=video) 
-    await reply.edit_text(f"**⥣ Uploading ...** » `")
-    await app.send_video(chat_id=message.chat.id, video=video, supports_streaming=True, duration=dur, progress=progress_bar, progress_args=(reply, start_time))    
-  #  os.remove(f"{video_id}.jpg") 
-
-def duration(filename):
-    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
-                             "format=duration", "-of",
-                             "default=noprint_wrappers=1:nokey=1", filename],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT)
-    return float(result.stdout)
+    start_time = time.time()
+    await reply.edit_text(f"**UPLOADING ...** » ")
+    await app.send_video(chat_id=message.chat.id, video=video, supports_streaming=True,  progress=progress_bar, progress_args=(reply, start_time))    
+ 
 
 
 @app.on_message((filters.document | filters.video | filters.photo) & filters.private)
