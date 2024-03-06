798c05a913e6ca450f4a94768758b3897c508262
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-03-06 16:12:51 +0530

    Update main.py

--- Watermark/modules/main.py
+++ Watermark/modules/main.py
@@ -4,13 +4,12 @@ from config import OWNER_ID
 
 @app.on_message((filters.document | filters.video | filters.photo) & filters.private)
 async def watcher(app, message):
-    if message.photo or (message.document and message.document.mime_type.startswith("image/")):
-        await message.reply_text("yes its a photo !!")
-        
-    elif message.video or (message.document and message.document.mime_type.startswith("video/")):
-        await message.reply_text("yes its a video")
-    else:
-        await message.reply_text("htt bsdk !!")
-        
-
+    user_id = message.from_user.id
+    if user_id in OWNER_ID:
+        if message.photo or (message.document and message.document.mime_type.startswith("image/")):
+            await message.reply_text("yes its a photo !!")
+        elif message.video or (message.document and message.document.mime_type.startswith("video/")):
+            await message.reply_text("yes its a video")
+        else:
+            await message.reply_text("htt bsdk !!")
     
