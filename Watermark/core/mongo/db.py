110a080dfe2ea3c6936fcfbe48b50ffbc112e9a0
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-03-06 22:07:30 +0530

    Update db.py

--- Watermark/core/mongo/db.py
+++ Watermark/core/mongo/db.py
@@ -6,33 +6,6 @@ db = _client["watermark"]
 col = db.user
 
 
-
-def new_user(id):
-    return dict(
-        _id=int(id),                                   
-        file_id=None,
-        caption=None
-    )
-
-async def add_user(id):
-    user = new_user(id)
-    await col.insert_one(user)
-
-async def is_user_exist(id):
-    user = await col.find_one({'_id': int(id)})
-    return bool(user)
-
-async def total_users_count():
-    count = await col.count_documents({})
-    return count
-
-async def get_all_users():
-    all_users = col.find({})
-    return all_users
-
-async def delete_user(user_id):
-    await col.delete_many({'_id': int(user_id)})
-
 async def set_thumbnail(id, file_id):
     await col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
 
@@ -47,5 +20,12 @@ async def get_caption(id):
     user = await col.find_one({'_id': int(id)})
     return user.get('caption', None)
 
+
+async def set_watermark(id, watermark):
+    await col.update_one({'_id': int(id)}, {'$set': {'watermark': watermark}})
+
+async def get_thumbnail(id):
+    user = await col.find_one({'_id': int(id)})
+    return user.get('watermark', None)
 
 
