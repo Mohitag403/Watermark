1a476cf56013c13e1b8875b1c8ea6add725d88c8
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-03-06 16:04:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+API_ID = int(getenv("API_ID", "18618422"))
+API_HASH = getenv("API_HASH", "f165b1caec3cfa4df943fe1cbe82d22a")
+BOT_TOKEN = getenv("BOT_TOKEN", "6954921683:AAGoxprOu-r85fS-5nL2sNAqNA9c_oh-WiU")
+OWNER_ID = int(getenv("OWNER_ID", "6050277919"))
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://mohit18324:TxsMAm4VjmS0nQ74@cluster0.ynzyhrh.mongodb.net/?retryWrites=true&w=majority")
