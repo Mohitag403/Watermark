96d3d8e372868475ae12d34e0d558310aef8eb20
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-03-07 00:20:30 +0530

    Update main.py

--- Watermark/modules/main.py
+++ Watermark/modules/main.py
@@ -16,7 +16,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 
 
 
-@app.on_message((filters.document | filters.video | filters.photo) & filters.private & filters.reply)
+@app.on_message((filters.document | filters.video | filters.photo) & filters.private)
 async def watcher(app, message):
     if message.photo or (message.document and message.document.mime_type.startswith("photo/")):
         photo = await message.download()
