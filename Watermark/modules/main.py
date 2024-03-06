5047702dfe4d1fabbf710980f3492bbc406e5f79
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-03-06 22:48:39 +0530

    Update main.py

--- Watermark/modules/main.py
+++ Watermark/modules/main.py
@@ -5,7 +5,6 @@ import subprocess
 from Watermark import app
 from config import OWNER_ID
 from Watermark.core.utils import progress_bar
-from Watermark.modules import view_thumb, remove_thumb, add_thumb, add_caption, delete_caption, see_caption
 
 
 
@@ -31,58 +30,3 @@ async def watcher(app, message):
                
     else:
         pass
-
-
-buttons1 = InlineKeyboardMarkup([
-            [
-                InlineKeyboardButton("Thumbnail", callback_data="thumb"),
-                InlineKeyboardButton("Caption", callback_data="caption")
-            ]
-        ])
-
-buttons2 = InlineKeyboardMarkup([
-            [
-                InlineKeyboardButton("Set Thumbnail", callback_data="Sthumb"),
-                InlineKeyboardButton("Remove Thumbnail", callback_data="Rthumb")
-            ],
-            [
-                InlineKeyboardButton("View Thumbnail", callback_data="Vthumb"),
-            ]
-        ])
-
-buttons3 = InlineKeyboardMarkup([
-            [
-                InlineKeyboardButton("Set Caption", callback_data="Scaption"),
-                InlineKeyboardButton("Remove Caption", callback_data="Rcaption")
-            ]
-        ])
-
-
-@app.on_message(filters.command("settings") & filters.private)
-async def settings(_, message):
-    await messgae.reply_text("Choose from Below", reply_markup=buttons1)
-
-
-@app.on_callback_query()
-async def callback(_, query):
-    if query.data=="thumb":
-        await messgae.reply_text("Choose from Below", reply_markup=buttons2)
-
-    elif query.data=="caption":
-        caption = await see_caption(query)
-        await messgae.reply_text(f"Choose from Below\n\n**ʏᴏᴜʀ ᴄᴀᴘᴛɪᴏɴ:** `{caption}`", reply_markup=buttons3)
-
-    elif query.data=="Sthumb":
-        await add_thumb(query)
-
-    elif query.data=="Rthumb":
-        await remove_thumb(query)
-
-    elif query.data=="Vthumb":
-        await view_thumb(query)
-
-    elif query.data=="Scaption":
-        await add_caption(query)
-
-    elif query.data=="Rcaption":
-        await delete_caption(query)
