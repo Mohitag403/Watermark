49d34707f19a51a6518b38f58e7a603d570fa22e
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-03-06 23:26:15 +0530

    Update db.py

--- Watermark/core/mongo/db.py
+++ Watermark/core/mongo/db.py
@@ -1,31 +1,36 @@
-import motor.motor_asyncio
+import datetime
 from config import MONGO_DB
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-_client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB)
-db = _client["watermark"]
-col = db.user
-
-
-async def set_thumbnail(id, file_id):
-    await col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
-
-async def get_thumbnail(id):
-    user = await col.find_one({'_id': int(id)})
-    return user.get('file_id', None)
-
-async def set_caption(id, caption):
-    await col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
-
-async def get_caption(id):
-    user = await col.find_one({'_id': int(id)})
-    return user.get('caption', None)
+mongo = MongoCli(MONGO_DB)
+db = mongo.watermark
+db = db.watermark_db
 
 
-async def set_watermark(id, watermark):
-    await col.update_one({'_id': int(id)}, {'$set': {'watermark': watermark}})
 
-async def get_thumbnail(id):
-    user = await col.find_one({'_id': int(id)})
-    return user.get('watermark', None)
+async def set_thumbnail(user_id, thumb):
+    data = await get_thumbnail(user_id)
+    if data and data.get("_id"):
+        await db.update_one({"_id": user_id}, {"$set": {"thumb": thumb}})
+    else:
+        await db.insert_one({"_id": user_id, "thumb": thumb})
+
+async def get_thumbnail(user_id):
+    x = await db.find_one({"_id": user_id})
+    return x
+
+
+async def set_caption(user_id, caption):
+    data = await get_thumbnail(user_id)
+    if data and data.get("_id"):
+        await db.update_one({"_id": user_id}, {"$set": {"caption": caption}})
+    else:
+        await db.insert_one({"_id": user_id, "caption": caption})
+
+async def get_caption(user_id):
+    x = await db.find_one({"_id": user_id})
+    return x
+    
+
 
 
